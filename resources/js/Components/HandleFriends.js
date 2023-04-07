0bc45e23fa1d0743885ad1a11edcf21b2b312666
BOYER AXEL <axel.boyer2@etu.unistra.fr>
2023-04-07 09:11:29 +0200

    adding hook to scroll to bottom of the messages div && added css

--- resources/js/Components/HandleFriends.js
+++ resources/js/Components/HandleFriends.js
@@ -4,5 +4,3 @@ export const HandleFriends = ({ user }) => {
     useEffect(() => {}, []);
     return <div>FriendList</div>;
 };
-
-// export default React.memo(HandleFriends);
--- resources/js/Components/Subcomponents/Messages.js
+++ resources/js/Components/Subcomponents/Messages.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import instance from "../../Utils/configAxios";
 
 const Messages = ({
@@ -18,18 +18,38 @@ const Messages = ({
             });
     }, []);
 
+    /*  The scrollToBottomRef function takes in parameter a ref, that is a reference to the div message DOM element, allowing us to scroll to the bottom of its children.
+     * We're using a callback function because we want to make sure that the div message DOM element is rendered before.
+     */
+    const scrollToBottomRef = useCallback((currentNode) => {
+        if (currentNode) {
+            scrollToBottom(currentNode);
+        }
+    }, []);
+    // We don't want to access to node.current since node is already the current node.
+    const scrollToBottom = (node) => {
+        node.scrollIntoView({
+            behavior: "smooth",
+            block: "end",
+            inline: "nearest",
+        });
+    };
+
     if (!messages) return null;
     chatChannel.listen(`.chatmessage.${friendId}`, (msg, senderId) => {
-        setMessages([
-            ...messages,
-            {
-                message: msg.message,
-                user_id_sender: friendId,
-            },
-        ]);
+        if (senderId === friendId) {
+            setMessages([
+                ...messages,
+                {
+                    message: msg.message,
+                    user_id_sender: friendId,
+                },
+            ]);
+        }
     });
+
     return (
-        <div className="messages">
+        <div className="messages" ref={scrollToBottomRef}>
             {messages.map((message) => {
                 return (
                     <p
